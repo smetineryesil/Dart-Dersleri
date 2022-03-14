f7392bcb9722020dcc24d93929accd6df821b32a
Seyit Metin ERYEŞIL <seyitmetineryesil@gmail.com>
2022-03-14 11:38:51 +0300

    first commit

---
+++ genel/kisayollar.dart
@@ -0,0 +1,24 @@
+main(List<String> args) {
+  //CTRL+X keser, CTRL+C kopyalar, CTRL+V yapıştırır
+
+  print("SEYİT METİN ERYEŞİL");
+
+  //CTRL+Z geri alır, CTRL+shift+Z ileri sarar
+  //CTRL+C tüm satırı kopyalar
+  //CTRL +ok tuşları kelime kelime ilerlemeyi sağlar
+  //CTLR + SHİFT + oklar kelime kelime seçim yapar
+  //ALT+ yukarı aşağı oklar ile ilgili satır veya seçili alan taşınabilir.
+  //ALT+ click ile birden fazla imlece sahip olunabilir.
+  //CTRL + K + C seçili alanı yorum satırı yapar
+  //CTRL + Ö satırı yorum satırı yapar
+  //SHİFT + ALT + A seçili blogu yorum satırı yapar
+  //SHİFT +ALT +yukarı aşağı oklar satırı kopyalar
+  //CTRL+P dosya arar
+  //CTRL+ F Dosyanın içinde arama yapar
+  //ctrl + alt + f bul ve değiştir
+  //ctrl+ shift +f tüm dosyalarda arar
+  //ctrl +b sol menuyu açar ve kapatır
+  //ctrl + tab bir sonraki sekme, ctrl + shift + tab bir önceki sekme
+  //ctrl+ k + f seçili alanı, alt + shift +f tüm dosyayı formatlı gösterir
+  //ctrl +" terminali açar kapar
+}
