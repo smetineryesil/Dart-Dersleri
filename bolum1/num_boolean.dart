a94cfe479fa30da98cade0e8025a0088df38724d
Seyit Metin ERYEŞIL <seyitmetineryesil@gmail.com>
2022-03-14 12:56:41 +0300

    num_int_var_ögrenme

---
+++ bolum1/num_boolean.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  int yas = 34;
+  print(yas);
+  yas = 44;
+  print(yas);
+
+  num yil = 1988;
+  print(yil);
+
+  double sayi = 52;
+  print(sayi);
+
+  int kilo = 8.6.toInt();
+  print(kilo);
+
+  var sayi2 = 56;
+
+  int numara;
+  numara = 5;
+  print(numara + 9);
+
+  int? s1 = null;
+  s1 = 9;
+  print(s1 + 5);
+
+  int hexadecimalSayi = 0xAABBCC;
+  print(hexadecimalSayi);
+}
--- bolum1/veritipleri_degiskenler.dart
+++ bolum1/veritipleri_degiskenler.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 main(List<String> args) {
   int yas = 34645494864;
   double ortlama = 54564564.6;
@@ -13,4 +15,16 @@ main(List<String> args) {
   print(ad + "");
   print(ad + "in sevdiği renkler");
   print(ad + "in yaşı");
+
+  bool erkekMi = true;
+  bool issizMi = false;
+
+  print(erkekMi);
+  print(issizMi);
+
+  var okul = "ege";
+  var okulNumarasi = 1453;
+  var kedisiVarmi = true;
+  print(okulNumarasi);
+  
 }
