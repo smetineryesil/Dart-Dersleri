ae87761812126be0ebed24c2746a35e9781863a6
Seyit Metin ERYEŞIL <seyitmetineryesil@gmail.com>
2022-03-14 12:13:53 +0300

    string denemeler

--- bolum1/hello_world.dart
+++ bolum1/hello_world.dart
@@ -3,4 +3,9 @@ void main(List<String> args) {
   print("SEYİT METİN ERYEŞİL");
   print("3+5");
   print(3 + 5);
+  print("emre'nin dersleri");
+  print("emre" + "altunbilek");
+
+  //burada ilk defa dart kodu yazıyorum
+
 }
---
+++ bolum1/veritipleri_degiskenler.dart
@@ -0,0 +1,16 @@
+main(List<String> args) {
+  int yas = 34645494864;
+  double ortlama = 54564564.6;
+  num yil = 1998;
+
+  print(yas);
+  print(ortlama);
+
+  String ad = "metin";
+
+  print(ad);
+
+  print(ad + "");
+  print(ad + "in sevdiği renkler");
+  print(ad + "in yaşı");
+}
